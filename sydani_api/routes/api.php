51f99c52da519672846ad1d876ca9bb53008e5ae
Afuwape Ifeoluwa <hipheckt@gmail.com>
2021-09-04 13:34:30 +0100

    requirement 1 completed

--- sydani_api/routes/api.php
+++ sydani_api/routes/api.php
@@ -12,7 +12,22 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['prefix' => 'v1'], function () {
+// Route::group(['prefix' => 'v1'], function () {
+
+//     Route::post('/login', 'UsersController@login')->name('login');
+//     Route::post('/register', 'UsersController@register');
+//     Route::post('/admin-login', 'UsersController@login')->name('admin-login');
+//     Route::post('/reset-password', 'UsersController@resetPassword');
+//     Route::post('/change-password/{id}', 'UsersController@changePassword');
+//     Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
+
+
+//     Route::get('/search-users/{query}', 'UsersController@searchUsers')->name('search-users');
+//     Route::get('/users/list', 'UsersController@listUsers')->middleware('auth:api');  //list with authentication
+//     // Route::get('/users/list', 'UsersController@listUsers'); //list without authentication
+// });
+
+Route::group(['prefix' => ''], function () {
 
     Route::post('/login', 'UsersController@login')->name('login');
     Route::post('/register', 'UsersController@register');
@@ -25,4 +40,10 @@ Route::group(['prefix' => 'v1'], function () {
     Route::get('/search-users/{query}', 'UsersController@searchUsers')->name('search-users');
     Route::get('/users/list', 'UsersController@listUsers')->middleware('auth:api');  //list with authentication
     // Route::get('/users/list', 'UsersController@listUsers'); //list without authentication
+
+
+    // Sydani Requests
+    Route::get('/external-book/all-books', 'IceFireController@listAllBooks');
+
+    Route::get('/external-book', 'IceFireController@reqOne');
 });
