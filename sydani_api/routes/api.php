0cf17323c2aebf2f24f11dd645a8bd77a30b8ba9
Afuwape Ifeoluwa <hipheckt@gmail.com>
2021-09-04 15:06:03 +0100

    completed API

--- sydani_api/routes/api.php
+++ sydani_api/routes/api.php
@@ -12,38 +12,25 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// Route::group(['prefix' => 'v1'], function () {
-
-//     Route::post('/login', 'UsersController@login')->name('login');
-//     Route::post('/register', 'UsersController@register');
-//     Route::post('/admin-login', 'UsersController@login')->name('admin-login');
-//     Route::post('/reset-password', 'UsersController@resetPassword');
-//     Route::post('/change-password/{id}', 'UsersController@changePassword');
-//     Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
-
-
-//     Route::get('/search-users/{query}', 'UsersController@searchUsers')->name('search-users');
-//     Route::get('/users/list', 'UsersController@listUsers')->middleware('auth:api');  //list with authentication
-//     // Route::get('/users/list', 'UsersController@listUsers'); //list without authentication
-// });
 
 Route::group(['prefix' => ''], function () {
 
-    Route::post('/login', 'UsersController@login')->name('login');
-    Route::post('/register', 'UsersController@register');
-    Route::post('/admin-login', 'UsersController@login')->name('admin-login');
-    Route::post('/reset-password', 'UsersController@resetPassword');
-    Route::post('/change-password/{id}', 'UsersController@changePassword');
-    Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
-
-
-    Route::get('/search-users/{query}', 'UsersController@searchUsers')->name('search-users');
-    Route::get('/users/list', 'UsersController@listUsers')->middleware('auth:api');  //list with authentication
-    // Route::get('/users/list', 'UsersController@listUsers'); //list without authentication
-
-
-    // Sydani Requests
+    // Sydani IceFire Requests
     Route::get('/external-book/all-books', 'IceFireController@listAllBooks');
 
     Route::get('/external-book', 'IceFireController@reqOne');
+});
+
+
+Route::group(['prefix' => 'v1'], function () {
+
+    // Sydani Local Books Design
+    Route::post('/books', 'BookController@createBook')->name('create');
+
+    Route::get('/books', 'BookController@listBooks')->name('books');
+
+    Route::post('/books/{id}', 'BookController@updateBook')->name('update');
+    
+    Route::get('/books/{id}', 'BookController@deleteBook')->name('delete');
+
 });
